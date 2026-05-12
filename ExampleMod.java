3d2c9c8ae08f2b3c28697b71fad2202aa756f734
apteczkaft <k.legionmoskva@gmail.com>
2026-05-12 15:05:50 +0200

    Update ExampleMod.java

--- ExampleMod.java
+++ ExampleMod.java
@@ -1,24 +1,31 @@
-package com.example;
+package net.fabricmc.example;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.network.chat.Component;
+import org.lwjgl.glfw.GLFW;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public class ExampleMod implements ClientModInitializer {
+    private static KeyMapping keyBinding;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+    @Override
+    public void onInitializeClient() {
+        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "Включить Автобай", 
+                GLFW.GLFW_KEY_R, 
+                "FunTime Helper"
+        ));
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (keyBinding.consumeClick()) {
+                AutoBuy.enabled = !AutoBuy.enabled;
+                if (client.player != null) {
+                    client.player.displayClientMessage(Component.literal("§6[Bot] §fАвтобай: " + (AutoBuy.enabled ? "§aВКЛ" : "§cВЫКЛ")), true);
+                }
+            }
+            AutoBuy.onTick(client);
+        });
+    }
 }
